609ff9f4cbd922f3415b769ad1ab5094f6db2f08
miles <miles4599@gmail.com>
2023-12-17 20:27:12 -0800

    updated gitignore

--- server/server.js
+++ server/server.js
@@ -1,3 +1,27 @@
+const express = require('express');
+const pool = require('./db'); // Path to your db.js file
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM idioms'); // Replace with your actual SQL query
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 // const express = require('express');
 // const { Pool } = require('pg');
 // const app = express();
