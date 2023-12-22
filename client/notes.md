153f4f6f49679d4b92f0690b6d9394824268820b
miles <miles4599@gmail.com>
2023-12-21 19:06:10 -0800

    Created react routes and pages

---
+++ client/notes.md
@@ -0,0 +1,23 @@
+`imr`
+
+```js
+import React from 'react';
+```
+
+`imrd`
+
+```js
+import ReactDOM from 'react-dom';
+```
+
+`rafce`
+
+```js
+import React from 'react';
+
+const UpdatePage = () => {
+  return <div>UpdatePage</div>;
+};
+
+export default UpdatePage;
+```
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './routes/HomePage';
+import DetailPage from './routes/DetailPage';
+import UpdatePage from './routes/UpdatePage';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/idioms/:id" element={<DetailPage />} />
+          <Route path="/idioms/:id/update" element={<UpdatePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
