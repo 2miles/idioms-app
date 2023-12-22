92c14560db50ff43aa4abbf2e5d5d39223033abd
miles <miles4599@gmail.com>
2023-12-22 14:23:20 -0800

    fixed key error

--- client/src/components/IdiomList.jsx
+++ client/src/components/IdiomList.jsx
@@ -18,7 +18,7 @@ const IdiomList = (props) => {
     <div>
       <table className="table table-bordered table-hover table-dark">
         <thead>
-          <tr className="big-primary">
+          <tr key={idioms.id} className="big-primary">
             <th> </th>
             <th>Title</th>
             <th>Definition</th>
@@ -27,46 +27,22 @@ const IdiomList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {idioms.map((idiom) => {
-            return (
-              <tr>
-                <td> {idiom.id}</td>
-                <td> {idiom.title_old}</td>
-                <td> {idiom.definition}</td>
-                <td>
-                  <button className="btn btn-secondary">Edit</button>
-                </td>
-                <td>
-                  <button className="btn btn-danger">Delete</button>
-                </td>
-              </tr>
-            );
-          })}
-          {/* <tr>
-            <td>Test Name</td>
-            <td>
-              When one tests ones name. Usually out of the blue but not always.
-            </td>
-            <td>
-              <button classname="btn btn-warning">Update</button>
-            </td>
-            <td>
-              <button classname="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Another test Name</td>
-            <td>
-              When another tests ones name. Usually out of the blue but not
-              always.
-            </td>
-            <td>
-              <button classname="btn btn-warning">Update</button>
-            </td>
-            <td>
-              <button classname="btn btn-danger">Delete</button>
-            </td>
-          </tr> */}
+          {idioms &&
+            idioms.map((idiom) => {
+              return (
+                <tr>
+                  <td> {idiom.id}</td>
+                  <td> {idiom.title_old}</td>
+                  <td> {idiom.definition}</td>
+                  <td>
+                    <button className="btn btn-secondary">Edit</button>
+                  </td>
+                  <td>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
