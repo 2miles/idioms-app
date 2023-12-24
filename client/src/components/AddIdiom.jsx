6876b4c236191b56dda561760badca8883d23e70
miles <miles4599@gmail.com>
2023-12-24 01:11:04 -0800

    ability to add idiom

--- client/src/components/AddIdiom.jsx
+++ client/src/components/AddIdiom.jsx
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import IdiomFinder from '../apis/idiomFinder';
+import { IdiomsContext } from '../context/idiomsContext';
 
 const AddIdiom = () => {
+  const { addIdioms } = useContext(IdiomsContext);
+  const [title, setTitle] = useState('');
+  const [titleNew, setTitleNew] = useState('');
+  const [definition, setDefinition] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await IdiomFinder.post('/', {
+        title_old: title,
+        title_new: titleNew,
+        definition: definition,
+      });
+      addIdioms(response.data.data.idioms);
+    } catch (err) {}
+  };
+
   return (
     <div className="mb-4 mx-4">
       <form action="">
         <div className="row">
           <div className="col-sm">
-            <input className="form-control" placeholder="Title" type="text" />
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="form-control"
+              placeholder="Title"
+              type="text"
+            />
           </div>
           <div className="col-sm">
             <input
+              value={titleNew}
+              onChange={(e) => setTitleNew(e.target.value)}
               className="form-control"
-              placeholder="Title General"
               type="text"
+              placeholder="Title General"
             />
           </div>
           <div className="col-sm-4">
             <input
+              value={definition}
+              onChange={(e) => setDefinition(e.target.value)}
               className="form-control"
               placeholder="Definition"
               type="text"
             />
           </div>
           <div className="col-sm">
-            <button className="btn btn-primary">Add</button>
+            <button
+              className="btn btn-primary"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Add
+            </button>
           </div>
         </div>
       </form>
--- client/src/context/idiomsContext.js
+++ client/src/context/idiomsContext.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useState, createContext } from 'react';
+ 
+export const IdiomsContext = createContext();
 
-export const IdiomsContext = createContext()
 
 export const IdiomsContextProvider = props =>  {
-  const [idioms, setIdioms] = useState([])
+  const [idioms, setIdioms] = useState([]);
+
+  const addIdioms = (idiom) => {
+    setIdioms([...idioms, idiom])
+  }
+
   return (
-    <IdiomsContext.Provider value={{idioms, setIdioms}}>
+    <IdiomsContext.Provider value={{idioms, setIdioms, addIdioms}}>
       {props.children}
     </IdiomsContext.Provider>
   )
