a4465e53443dbb8fba5754c469c95db32f0142e3
miles <miles4599@gmail.com>
2023-12-31 15:30:49 -0800

    new context and idiom detail page

--- client/src/context/idiomsContext.js
+++ client/src/context/idiomsContext.js
@@ -6,13 +6,14 @@ export const IdiomsContext = createContext();
 
 export const IdiomsContextProvider = props =>  {
   const [idioms, setIdioms] = useState([]);
+  const [selectedIdiom, setSelectedIdiom] = useState(null);
 
   const addIdioms = (idiom) => {
     setIdioms([...idioms, idiom])
   }
 
   return (
-    <IdiomsContext.Provider value={{idioms, setIdioms, addIdioms}}>
+    <IdiomsContext.Provider value={{idioms, setIdioms, addIdioms, selectedIdiom, setSelectedIdiom}}>
       {props.children}
     </IdiomsContext.Provider>
   )
--- client/src/routes/DetailPage.jsx
+++ client/src/routes/DetailPage.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { IdiomsContext } from '../context/idiomsContext';
+import idiomFinder from '../apis/idiomFinder';
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  const { id } = useParams();
+  const { selectedIdiom, setSelectedIdiom } = useContext(IdiomsContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await idiomFinder.get(`/${id}`);
+        //console.log(response.data.data.idiom);
+        setSelectedIdiom(response.data.data.idiom);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>{selectedIdiom && selectedIdiom.title_old}</h1>
+      <h2>{selectedIdiom && selectedIdiom.title_new}</h2>
+      <p>{selectedIdiom && selectedIdiom.definition}</p>
+    </div>
+  );
 };
 
 export default DetailPage;
