fe4ce2cf9723f8e124005a0b89ccc7c568b64818
miles8 <miles8@pdx.edu>
2024-03-14 22:29:40 -0700

    Add comments for learning

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,7 @@
+ // Im not sure exactly why this is in its own file and not just at the top of server.js
+
 const { Pool } = require('pg');
-
+ // The pg library is commonly used with Node.js for interacting with PostgreSQL databases.
+ // Pool is a pg class that manages a pool of client connections to the database.
 const pool = new Pool();
-
 module.exports = pool;
