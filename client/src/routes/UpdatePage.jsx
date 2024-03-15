3838ace701c5c84af948597dd5b62afcaaefee66
miles8 <miles8@pdx.edu>
2024-03-14 22:24:34 -0700

    Make update page text white

--- client/src/routes/UpdatePage.jsx
+++ client/src/routes/UpdatePage.jsx
@@ -4,7 +4,7 @@ import UpdateIdiom from '../components/UpdateIdiom';
 const UpdatePage = () => {
   return (
     <div>
-      <h1 className="text-center">Update Idiom</h1>
+      <h1 className="text-center text-white">Update Idiom</h1>
       <UpdateIdiom />
     </div>
   );
