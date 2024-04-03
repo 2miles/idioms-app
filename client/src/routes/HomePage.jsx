4bb43f840d4368cf8d1e577c5a2d61926aed389b
miles <miles4599@gmail.com>
2024-04-02 20:24:15 -0700

    delete 'table.jsx'

--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import AddIdiom from '../components/AddIdiom';
 import IdiomList from '../components/IdiomList';
-import Table from '../components/table';
+import Table from '../components/Table';
 
 const HomePage = () => {
   return (
