9e1bd15a9a58f316d5b222efdd81daa5d2284174
miles <miles4599@gmail.com>
2024-04-04 21:50:04 -0700

    Move most table logic into HomePage component.

--- client/src/components/Table_.jsx
+++ client/src/components/Table_.jsx
@@ -1,36 +1,9 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import TableBody from './TableBody';
 import TableHead from './TableHead';
-import IdiomFinder from '../apis/idiomFinder';
 import SearchBar from './SearchBar';
 
-const Table = () => {
-  const [tableData, setTableData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [idiomCount, setIdiomCount] = useState(0); // State to store the count of filtered idioms
-
-  // Will execute this code block only once, right after the component is initially rendered.
-  // If the dependency array is empty, it won't run again on subsequent renders.
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await IdiomFinder.get('/');
-        setTableData(response.data.data.idioms);
-        setFilteredData(response.data.data.idioms);
-        setIdiomCount(response.data.data.idioms.length);
-      } catch (err) {}
-    };
-    fetchData();
-  }, []);
-
-  const handleSearch = (searchTerm) => {
-    const filtered = tableData.filter((item) =>
-      item.title_old.toLowerCase().includes(searchTerm.toLowerCase()),
-    );
-    setFilteredData(filtered);
-    setIdiomCount(filtered.length);
-  };
-
+const Table = ({ tableData, idiomCount, handleSearch, handleSorting }) => {
   const columns = [
     { label: 'ID', accessor: 'id', sortable: true },
     { label: 'Title', accessor: 'title_old', sortable: true },
@@ -38,22 +11,6 @@ const Table = () => {
     { label: 'Day', accessor: 'day', sortable: true },
     { label: 'Owner', accessor: 'owner', sortable: true },
   ];
-
-  const handleSorting = (sortField, sortOrder) => {
-    if (sortField) {
-      const sorted = [...filteredData].sort((a, b) => {
-        if (a[sortField] === null) return 1;
-        if (b[sortField] === null) return -1;
-        if (a[sortField] === null && b[sortField] === null) return 0;
-        return (
-          a[sortField].toString().localeCompare(b[sortField].toString(), 'en', {
-            numeric: true,
-          }) * (sortOrder === 'asc' ? 1 : -1)
-        );
-      });
-      setFilteredData(sorted);
-    }
-  };
 
   return (
     <>
@@ -63,7 +20,7 @@ const Table = () => {
       </div>
       <table className="table">
         <TableHead columns={columns} handleSorting={handleSorting} />
-        {tableData && <TableBody columns={columns} tableData={filteredData} />}
+        {tableData && <TableBody columns={columns} tableData={tableData} />}
       </table>
     </>
   );
--- client/src/routes/HomePage.jsx
+++ client/src/routes/HomePage.jsx
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import AddIdiom from '../components/AddIdiom';
 import Table from '../components/Table_';
+import IdiomFinder from '../apis/idiomFinder';
 
 const HomePage = () => {
+  const [tableData, setTableData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [idiomCount, setIdiomCount] = useState(0); // State to store the count of filtered idioms
+
+  // Will execute this code block only once, right after the component is initially rendered.
+  // If the dependency array is empty, it won't run again on subsequent renders.
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await IdiomFinder.get('/');
+        const data = response.data.data.idioms;
+        setTableData(data);
+        setFilteredData(data);
+        setIdiomCount(data.length);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleSearch = (searchTerm) => {
+    const filtered = tableData.filter((item) =>
+      item.title_old.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredData(filtered);
+    setIdiomCount(filtered.length);
+  };
+
+  const handleSorting = (sortField, sortOrder) => {
+    if (sortField) {
+      const sorted = [...filteredData].sort((a, b) => {
+        if (a[sortField] === null) return 1;
+        if (b[sortField] === null) return -1;
+        if (a[sortField] === null && b[sortField] === null) return 0;
+        return (
+          a[sortField].toString().localeCompare(b[sortField].toString(), 'en', {
+            numeric: true,
+          }) * (sortOrder === 'asc' ? 1 : -1)
+        );
+      });
+      setFilteredData(sorted);
+    }
+  };
+
   return (
     <div className="table_container">
       <Header />
       <AddIdiom />
-      <Table />
+      <Table
+        tableData={filteredData}
+        idiomCount={idiomCount}
+        handleSearch={handleSearch}
+        handleSorting={handleSorting}
+      />
     </div>
   );
 };
