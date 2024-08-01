ff914b074d6c2e8b20f9909e3be66b0c6eb77bed
miles <miles4599@gmail.com>
2024-08-01 13:44:03 -0700

    Update axios baseURL

--- client/src/apis/idiomFinder.js
+++ client/src/apis/idiomFinder.js
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios from 'axios';
 
 // With Axios, you don't need to manually check for response.ok and handle JSON parsing,
 // as Axios automatically handles these for you.
-export default axios.create( {
-  baseURL: "http://localhost:3001/api/v1/idioms"
-})
+export default axios.create({
+  // baseURL: "http://localhost:3001/api/v1/idioms"
+  baseURL: 'http://192.168.0.100:3001/api/v1/idioms',
+});
