890d92796e7aabf6e54e2180aba0030df29385a6
miles <miles4599@gmail.com>
2024-08-27 20:26:00 -0700

    Dev can now choose between local using db or Supabase

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,23 @@
- // Im not sure exactly why this is in its own file and not just at the top of server.js
-
 const { Pool } = require('pg');
- // The pg library is commonly used with Node.js for interacting with PostgreSQL databases.
- // Pool is a pg class that manages a pool of client connections to the database.
-const pool = new Pool();
+require('dotenv').config();
+
+let pool;
+
+if (process.env.USE_SUPABASE === 'true') {
+  pool = new Pool({
+    connectionString: process.env.DB_URL_SUPABASE,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  pool = new Pool({
+    host: process.env.LOCAL_PSQL_HOST,
+    port: process.env.LOCAL_PSQL_PORT,
+    user: process.env.LOCAL_PSQL_USER,
+    password: process.env.LOCAL_PSQL_PASSWORD,
+    database: process.env.LOCAL_PSQL_DATABASE,
+  });
+}
+
 module.exports = pool;
