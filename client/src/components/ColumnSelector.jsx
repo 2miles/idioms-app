d84a3809637b8be1db227bd26fe604e524a95935
miles <miles4599@gmail.com>
2024-09-06 15:58:10 -0700

    colum, items per page selector components now using styled-components

--- client/src/components/ColumnSelector.jsx
+++ client/src/components/ColumnSelector.jsx
@@ -1,4 +1,78 @@
 import React, { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const ColumnSelectDropdown = styled.div`
+  position: relative;
+  display: flex;
+  cursor: pointer;
+  user-select: none;
+  border: 1px solid #e9ecef;
+  background-color: #f8f9fa;
+  border-radius: 5px;
+  margin-right: 20px;
+  margin-bottom: 1rem;
+  padding: 0px 10px;
+  height: 35px;
+
+  @media (max-width: 770px) {
+    display: none;
+  }
+
+  .items {
+    display: none;
+    position: absolute;
+    top: calc(100% + 5px);
+    right: 0;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    z-index: 1;
+    padding: 0;
+    list-style: none;
+  }
+
+  &.visible .items {
+    display: block;
+  }
+`;
+
+const Anchor = styled.span`
+  display: flex;
+  align-items: center;
+
+  &::after {
+    content: '';
+    width: 0;
+    height: 0;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    border-top: 6px solid black;
+    margin-left: 10px;
+  }
+
+  .visible &::after {
+    border-top: none;
+    border-bottom: 6px solid black;
+  }
+`;
+
+const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+
+  input[type='checkbox'] {
+    margin-right: 10px; /* Add margin to the checkbox */
+  }
+`;
+
+const ListItem = styled.li`
+  padding: 5px 10px;
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
 
 const ColumnSelector = ({ columnVisibility, handleColumnVisibilityChange }) => {
   const [showColumnSelector, setShowColumnSelector] = useState(false);
@@ -19,27 +93,27 @@ const ColumnSelector = ({ columnVisibility, handleColumnVisibilityChange }) => {
   }, [showColumnSelector]);
 
   return (
-    <div
-      className={`dropdown-check-list ${showColumnSelector ? 'visible' : ''}`}
-      ref={dropdownRef} // Attach ref to the dropdown container
+    <ColumnSelectDropdown
+      className={showColumnSelector ? 'visible' : ''}
+      ref={dropdownRef}
       onClick={() => setShowColumnSelector(!showColumnSelector)}
     >
-      <span className="anchor">Columns</span>
+      <Anchor>Columns</Anchor>
       <ul className="items" onClick={(e) => e.stopPropagation()}>
         {Object.keys(columnVisibility).map((column) => (
-          <li key={column}>
-            <label className="checkbox-label">
+          <ListItem key={column}>
+            <CheckboxLabel>
               <input
                 type="checkbox"
                 checked={columnVisibility[column]}
                 onChange={() => handleColumnVisibilityChange(column)}
               />
               {column.charAt(0).toUpperCase() + column.slice(1)}
-            </label>
-          </li>
+            </CheckboxLabel>
+          </ListItem>
         ))}
       </ul>
-    </div>
+    </ColumnSelectDropdown>
   );
 };
 
--- client/src/components/ItemsPerPageSelector.jsx
+++ client/src/components/ItemsPerPageSelector.jsx
@@ -1,4 +1,62 @@
 import React, { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
+
+const ItemsPerPageDropdown = styled.div`
+  position: relative;
+  display: flex;
+  cursor: pointer;
+  user-select: none;
+  border: 1px solid #e9ecef;
+  background-color: #f8f9fa;
+  border-radius: 5px;
+  margin-right: 20px;
+  margin-bottom: 1rem;
+  padding: 0px 10px;
+  height: 35px;
+`;
+
+const Selected = styled.span`
+  display: flex;
+  align-items: center;
+
+  &:after {
+    content: '';
+    width: 0;
+    height: 0;
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    border-top: 6px solid black;
+    margin-left: 10px;
+  }
+  .visible &::after {
+    border-top: none;
+    border-bottom: 6px solid black;
+  }
+`;
+
+const Options = styled.div`
+  display: block;
+  position: absolute;
+  top: calc(100% + 5px);
+  right: 0;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+  width: 100%;
+  max-height: 200px;
+  overflow-y: auto;
+`;
+
+const Option = styled.div`
+  padding: 5px 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
 
 const ItemsPerPageSelector = ({ itemsPerPage, handleItemsPerPageChange }) => {
   const [showOptions, setShowOptions] = useState(false);
@@ -28,26 +86,22 @@ const ItemsPerPageSelector = ({ itemsPerPage, handleItemsPerPageChange }) => {
   };
 
   return (
-    <div
-      className="custom-dropdown"
+    <ItemsPerPageDropdown
+      className={showOptions ? 'visible' : ''}
       ref={dropdownRef}
       onClick={() => setShowOptions(!showOptions)}
     >
-      <span className="selected">{selectedOption} </span>
+      <Selected>{selectedOption} </Selected>
       {showOptions && (
-        <div className="options">
+        <Options>
           {options.map((option) => (
-            <div
-              key={option}
-              className="option"
-              onClick={() => handleOptionClick(option)}
-            >
+            <Option key={option} onClick={() => handleOptionClick(option)}>
               {option}
-            </div>
+            </Option>
           ))}
-        </div>
+        </Options>
       )}
-    </div>
+    </ItemsPerPageDropdown>
   );
 };
 
