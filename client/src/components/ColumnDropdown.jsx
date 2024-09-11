05b42b8f8c93ddb218b44f62ffc18046778f60f9
miles <miles4599@gmail.com>
2024-09-11 14:49:06 -0700

    Remove old comments

--- client/src/components/ColumnDropdown.jsx
+++ client/src/components/ColumnDropdown.jsx
@@ -1,42 +1,7 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import Dropdown from './Dropdown'; // Import base Dropdown
-
-// const CheckboxLabel = styled.label`
-//   display: flex;
-//   align-items: center;
-//   cursor: pointer;
-
-//   input[type='checkbox'] {
-//     margin-right: 10px;
-//   }
-// `;
-
-// const DropdownCheckbox = ({ options, selected, handleChange, label }) => {
-//   return (
-//     <Dropdown label={label}>
-//       {options.map((option) => (
-//         <li key={option}>
-//           <CheckboxLabel>
-//             <input
-//               type="checkbox"
-//               checked={selected[option] || false}
-//               onChange={() => handleChange(option)}
-//             />
-//             {option.charAt(0).toUpperCase() + option.slice(1)}
-//           </CheckboxLabel>
-//         </li>
-//       ))}
-//     </Dropdown>
-//   );
-// };
-
-// export default ColumnDropdown;
 import React from 'react';
 import Dropdown from './Dropdown';
 import styled from 'styled-components';
 
-// Styled components for checkbox labels
 const CheckboxLabel = styled.label`
   display: flex;
   align-items: center;
