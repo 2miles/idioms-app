61c0dcf862e04080cd7649fa59fc14cf42940d6b
miles <miles4599@gmail.com>
2024-09-12 18:52:13 -0700

    DetailPage using styled-components

--- client/src/routes/DetailPage.jsx
+++ client/src/routes/DetailPage.jsx
@@ -3,6 +3,63 @@ import IdiomFinder from '../apis/idiomFinder';
 import { useParams } from 'react-router-dom';
 import { IdiomsContext } from '../context/idiomsContext';
 import UpdateIdiom from '../components/UpdateIdiom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  &.card {
+    background: linear-gradient(
+      to bottom right,
+      rgba(255, 255, 255, 0.5),
+      rgba(230, 230, 230, 0.7)
+    );
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    width: 100%;
+    margin: 20px auto;
+    border: 1px solid #e0e0e0;
+    font-family: 'Times New Roman', Times, serif;
+    overflow: hidden;
+  }
+`;
+
+const CardHeader = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+
+  h1 {
+    font-size: 2em;
+    margin: 0;
+    padding: 20px;
+    font-style: italic;
+    font-weight: normal;
+  }
+
+  h2 {
+    font-size: 1.5em;
+    margin: 0;
+    font-style: italic;
+    font-weight: normal;
+    color: #555;
+  }
+`;
+const CardBody = styled.div`
+  font-size: 1.2em;
+  line-height: 1.6;
+  padding-left: 50px;
+  padding-right: 50px;
+
+  h3 {
+    margin-top: 30px;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+
+  @media (max-width: 770px) {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+`;
 
 const DetailPage = () => {
   const { id } = useParams();
@@ -51,8 +108,8 @@ const DetailPage = () => {
         <p>Loading...</p>
       ) : (
         selectedIdiom && (
-          <div className="card">
-            <div className="card-header">
+          <Card className="card">
+            <CardHeader className="card-header">
               <h1>
                 &quot;
                 {selectedIdiom.title_general &&
@@ -61,21 +118,20 @@ const DetailPage = () => {
                   : selectedIdiom.title}
                 &quot;
               </h1>
-            </div>
-            <div className="card-body">
+            </CardHeader>
+            <CardBody className="card-body">
               <p># {selectedIdiom.id}</p>
-              {/* <p>Added on {selectedIdiom.timestamps?.substring(0, 10)}</p>
               <p>{selectedIdiom.contributor}</p> */}
               <h3>Meaning:</h3>
-              <p className="definition">{selectedIdiom.definition}</p>
+              <p>{selectedIdiom.definition}</p>
               <h3>Examples:</h3>
               <ul>
                 {examples.map((example) => (
                   <li key={example.example_id}>{example.example}</li>
                 ))}
               </ul>
-            </div>
-          </div>
+            </CardBody>
+          </Card>
         )
       )}
       <div>
