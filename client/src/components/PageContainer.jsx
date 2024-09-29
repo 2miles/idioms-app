ec066022cad26603bf8e681e8b9adc71834d508f
miles <miles4599@gmail.com>
2024-09-29 03:22:20 -0700

    Fix page container not centered

--- client/src/components/PageContainer.jsx
+++ client/src/components/PageContainer.jsx
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
 const PageContainer = styled.div`
-  max-width: 1400px;
+  max-width: 1200px;
   margin: 0 auto 200px;
-  margin-left: 100px;
-  margin-right: 100px;
 
   @media (max-width: 950px) {
     margin-left: 16px;
