69a24d409d657c11fba3a4ea956d69690f02cc30
miles <miles4599@gmail.com>
2025-04-06 16:14:18 -0700

    db/index.ts only uses Supabase now

--- server/db/index.ts
+++ server/db/index.ts
@@ -6,21 +6,11 @@ dotenv.config();
 
 let pool: pkg.Pool;
 
-if (process.env.USE_SUPABASE === 'true') {
-  pool = new Pool({
-    connectionString: process.env.DB_URL_SUPABASE_DEV as string,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  pool = new Pool({
-    host: process.env.LOCAL_PSQL_HOST as string,
-    port: parseInt(process.env.LOCAL_PSQL_PORT as string, 10),
-    user: process.env.LOCAL_PSQL_USER as string,
-    password: process.env.LOCAL_PSQL_PASSWORD as string,
-    database: process.env.LOCAL_PSQL_DATABASE as string,
-  });
-}
+pool = new Pool({
+  connectionString: process.env.DB_URL_SUPABASE_DEV as string,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
