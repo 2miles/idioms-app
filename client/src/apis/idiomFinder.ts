7b4e1b19a03fefaa63dbcae3df93f21ab90e510c
miles <miles4599@gmail.com>
2025-04-14 14:10:11 -0700

    Update api baseURL in frontend to env variable

--- client/src/apis/idiomFinder.ts
+++ client/src/apis/idiomFinder.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001/api/v1/idioms',
+  // Latest version with environment variable
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1/idioms`,
 
+  // No environment variable version
+  //baseURL: 'http://localhost:3001/api/v1/idioms',
+
+  // If I want use my phone
   // baseURL: 'http://192.168.0.100:3001/api/v1/idioms',
+
+  // Not sure what this is about
   // baseURL: 'http://idioms-app-server:3001/api/v1/idioms',
 });
