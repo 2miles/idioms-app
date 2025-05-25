40833e486c25dcd29f099f92295f91e5b4f9b36c
miles <miles4599@gmail.com>
2025-05-24 22:34:01 -0700

    Added vite api base url log function

--- client/src/apis/idiomFinder.ts
+++ client/src/apis/idiomFinder.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/v1/idioms`;
+console.log('VITE_API_BASE_URL', import.meta.env.VITE_API_BASE_URL);
 
 export const publicIdiomFinder = axios.create({
   baseURL,
