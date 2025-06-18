ce9f7f6212db752ac60e4f0d7005881ae011e972
miles <miles4599@gmail.com>
2025-06-18 11:43:39 -0700

    style: clean up colors, update scheme, and fix table shrink on load for large screens

--- client/src/components/PageContainer.tsx
+++ client/src/components/PageContainer.tsx
@@ -2,16 +2,17 @@ import styled from 'styled-components';
 
 const PageContainer = styled.div`
   max-width: 1200px;
+  width: 100%;
   margin: 0 auto var(--space-xxxl);
 
   @media (max-width: 1300px) {
-    margin-left: var(--margin-xxl);
-    margin-right: var(--margin-xxl);
+    padding-left: var(--padding-xxl);
+    padding-right: var(--padding-xxl);
   }
 
   @media (max-width: 800px) {
-    margin-left: var(--margin-sm);
-    margin-right: var(--margin-sm);
+    padding-left: var(--padding-sm);
+    padding-right: var(--padding-sm);
   }
 `;
 
