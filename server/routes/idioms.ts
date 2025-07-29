9fc810b3c9d3f1487d7e6c88a37ac760cb54d82d
miles <miles4599@gmail.com>
2025-07-29 12:11:14 -0700

    refactor: Split server.ts into controller, router and server.

---
+++ server/routes/idioms.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import {
+  getAllIdioms,
+  getSingleIdiomWithExamples,
+  createIdiom,
+  updateIdiom,
+  deleteIdiom,
+  createExample,
+  updateExamples,
+  deleteExample,
+} from '../controllers/idiomsController.ts';
+
+import { jwtCheck, checkRole } from '../authMiddleware.ts';
+
+const router = express.Router();
+
+// Public
+router.get('/', getAllIdioms);
+router.get('/:id', getSingleIdiomWithExamples);
+
+// // Protected
+router.post('/', jwtCheck, checkRole('Admin'), createIdiom);
+router.put('/:id', jwtCheck, checkRole('Admin'), updateIdiom);
+router.delete('/:id', jwtCheck, checkRole('Admin'), deleteIdiom);
+
+router.post('/:id/examples', jwtCheck, checkRole('Admin'), createExample);
+router.put('/:id/examples', jwtCheck, checkRole('Admin'), updateExamples);
+router.delete('/examples/:id', jwtCheck, checkRole('Admin'), deleteExample);
+
+export default router;
