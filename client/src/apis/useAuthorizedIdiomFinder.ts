4b08aabdbde4446e8a372733fdc541a30cb8fc50
miles <miles4599@gmail.com>
2025-08-09 15:50:36 -0700

    remove auth token refresh

--- client/src/apis/useAuthorizedIdiomFinder.ts
+++ client/src/apis/useAuthorizedIdiomFinder.ts
@@ -2,21 +2,22 @@ import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/v1/idioms`;
+const audience = 'https://api.idiomvault.com';
+const scope = 'openid profile email';
 
 const useAuthorizedIdiomFinder = () => {
   const { getAccessTokenSilently } = useAuth0();
 
-  const getAxios = async () => {
-    const token = await getAccessTokenSilently();
+  return async () => {
+    const token = await getAccessTokenSilently({
+      authorizationParams: { audience, scope },
+    });
+
     return axios.create({
       baseURL,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+      headers: { Authorization: `Bearer ${token}` },
     });
   };
-
-  return getAxios;
 };
 
 export default useAuthorizedIdiomFinder;
